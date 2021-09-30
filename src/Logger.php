ecf925956b2ea94c9866f7a6360c18951a68171f
Iskander Sharipov <i.sharipov@corp.vk.com>
2021-09-30 11:48:56 +0300

    add simple logger class

---
+++ src/Logger.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KPHPGame;
+
+class Logger {
+  public static function info(string $message): void {
+    fprintf(STDERR, "INFO: %s\n", $message);
+  }
+
+  public static function error(string $message): void {
+    fprintf(STDERR, "ERROR: %s\n", $message);
+  }
+}
